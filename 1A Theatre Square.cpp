c9e8ce7fca06d62f33fe680954077cec568fc39d
AlfaroukSaleh <50967999+AlfaroukSaleh@users.noreply.github.com>
2019-05-23 23:55:14 +0200

    Create 1A Theatre Square.cpp

---
+++ 1A Theatre Square.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main (){
+  
+
+    long double n,m,a ;
+    long long x;
+    cin >>n>>m>>a;
+   x= ceil(n/a)*ceil (m/a);
+   cout<<x<<endl;
+    return 0;}
